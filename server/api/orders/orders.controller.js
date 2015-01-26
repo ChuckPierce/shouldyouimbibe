4c99840e9b5662c032d9e61a6251632197481e51
Chuck Pierce <chuckmpierce@gmail.com>
2015-01-25 21:30:03 -0500

    still can't get api to get what it wants

--- server/api/orders/orders.controller.js
+++ server/api/orders/orders.controller.js
@@ -58,14 +58,10 @@ exports.postToDrizly = function(req, res) {
           // console.log('response from drizly', body);
           var prepare = JSON.parse(body);
           var prepareToken = prepare.payment_client_token;
-          request.post({url: 'https://sandbox.drizly.com/api/v2/checkout/process?partner_token=301cc08e728c8ccaa377c5b76f6c773b&token='+ config.drizly.dToken.token+'&payment_client_token='+ prepareToken, formData: req.body}, function(err, response, payment) {
+          request.post({url: 'https://sandbox.drizly.com/api/v2/checkout/process?partner_token=301cc08e728c8ccaa377c5b76f6c773b&token='+ config.drizly.dToken.token+'&payment_client_token='+ prepareToken, formData: req.body }, function(err, response, payment) {
             return res.json(JSON.parse(payment));
           });
   });
-  // var thisBody = {
-  //   email: 'charlesmpierce@gmail.com',
-  //   zip: '10033'
-  // };
   // request.post({url: 'https://sandbox.drizly.com/api/v2/prospect?partner_token=301cc08e728c8ccaa377c5b76f6c773b&token='+ config.drizly.dToken.token, formData: thisBody}, function(err, response, body) {
   //   console.log(body);
   // });
